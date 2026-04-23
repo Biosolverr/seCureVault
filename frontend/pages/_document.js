3fd255663d05df5c665c168cc3def4b185d4d5c8
Biosolver <asperant001@gmail.com>
2026-04-23 18:27:10 +0700

    Update _document.js

--- frontend/pages/_document.js
+++ frontend/pages/_document.js
@@ -4,8 +4,14 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head>
+        {/* Base / dApp verification */}
         <meta name="cb-app-id" content="securevault" />
+
+        {/* (опционально, но полезно) */}
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
+
       <body>
         <Main />
         <NextScript />
